01bf3b0cf5523ac821fbb3a8555f52e3560cf4ca
ww2or3ww <w2or3w@gmail.com>
2020-06-15 10:44:34 +0000

    for locoguide

--- pages/fire.vue
+++ pages/fire.vue
@@ -4,7 +4,7 @@
       <p style="margin: 0px; padding: 0px;">
         【
         <a href="https://camp-fire.jp/projects/view/282243" target="_blank">
-          花火師応援プロジェクト
+          {{ $t('fire.link1') }}
         </a>
         】
       </p>
@@ -14,7 +14,7 @@
           href="https://www.google.com/search?q=%E6%97%A5%E6%9C%AC%E3%81%AE%E8%8A%B1%E7%81%AB%E3%80%8C%E3%82%A8%E3%83%BC%E3%83%AB%E3%80%8D%E3%83%97%E3%83%AD%E3%82%B8%E3%82%A7%E3%82%AF%E3%83%88"
           target="_blank"
         >
-          日本の花火「エール」プロジェクト
+          {{ $t('fire.link2') }}
         </a>
         】
       </p>
