a4f8b237f2848535c8ad7361d32d3d4ec538f885
ww2or3ww <w2or3w@gmail.com>
2020-07-12 13:28:48 +0000

    refactoring

--- pages/life.vue
+++ pages/life.vue
@@ -1,7 +1,10 @@
 <template>
   <v-layout style="margin: 0px; padding: 0px;" justify-center>
     <v-flex style="margin: 0px; padding: 0px;" xs12 sm12 md12>
-      <map-base ref="mapBase" :markers="markers" :has_clowd="false"></map-base>
+      <p style="margin: 0px; padding: 0px; font-size: 15px;">
+        {{ $t('life.text') }}
+      </p>
+      <map-base ref="mapBase" :markers="markers"></map-base>
     </v-flex>
   </v-layout>
 </template>
