fcd664b154fe21db2683bf51a3b69acd3a69529e
ww2or3ww <w2or3w@gmail.com>
2020-07-13 14:37:54 +0000

    change gmap option's style

--- components/MapBase.vue
+++ components/MapBase.vue
@@ -316,6 +316,14 @@ export default {
         streetViewControl: false,
         styles: [
           {
+            elementType: 'labels',
+            stylers: [
+              {
+                visibility: 'off'
+              }
+            ]
+          },
+          {
             featureType: 'administrative.land_parcel',
             elementType: 'labels',
             stylers: [
