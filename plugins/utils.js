78556b61290b092f6ecd5196b6312b4d0f729303
ww2or3ww <w2or3w@gmail.com>
2020-07-22 12:17:25 +0000

    delete fire

--- plugins/utils.js
+++ plugins/utils.js
@@ -4,73 +4,56 @@ export default (context, inject) => {
       {
         link: '/',
         icon: 'mdi-home',
-        count: '',
         hilite: false
       },
       {
         link: '/loco',
         icon: 'mdi-lightbulb-on-outline',
-        count: '(---)',
         hilite: true
       },
       {
         link: '/food',
         icon: 'mdi-food',
-        count: '(624)',
         hilite: false
       },
       {
         link: '/drink',
         icon: 'mdi-coffee',
-        count: '(74)',
         hilite: false
       },
       {
         link: '/life',
         icon: 'mdi-shopping-outline',
-        count: '(117)',
         hilite: false
       },
       {
         link: '/mask',
         icon: 'mdi-alpha-m-circle-outline',
-        count: '(50)',
         hilite: false
-      },
-      {
-        link: '/fire',
-        icon: 'mdi-fire',
-        count: '(325)',
-        hilite: true
       },
       {
         link: '/outdoor',
         icon: 'mdi-tent',
-        count: '(61)',
         hilite: false
       },
       {
         link: '/hotspring',
         icon: 'mdi-hot-tub',
-        count: '(142)',
         hilite: false
       },
       {
         link: '/plant',
         icon: 'mdi-flower',
-        count: '(0)',
         hilite: false
       },
       {
         link: '/hamapay',
         icon: 'mdi-shopping-outline',
-        count: '(---)',
         hilite: true
       },
       {
         link: '/about',
         icon: 'mdi-map-marker-question-outline',
-        count: '',
         hilite: false
       }
     ]
