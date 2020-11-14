70c31ae1b5e428c7ddf3e735bfa8815d344e5cc6
ww2or3ww <w2or3w@gmail.com>
2020-11-14 13:56:42 +0000

    temple

---
+++ pages/temple.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-layout style="margin: 0px; padding: 0px;" justify-center>
+    <v-flex style="margin: 0px; padding: 0px;" xs12 sm12 md12>
+      <map-base ref="mapBase"></map-base>
+    </v-flex>
+  </v-layout>
+</template>
+<script>
+import MapBase from '@/components/MapBase.vue'
+export default {
+  components: {
+    MapBase
+  },
+  mounted() {
+    this.$refs.mapBase.initialize('temple')
+  }
+}
+</script>
